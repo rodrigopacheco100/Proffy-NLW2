63274da4a02708d15cd34b72bc52147c88663137
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2020-08-04 11:01:33 -0300

    commit

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const TeacherForm: React.FC = () => {
+   return <h1>TeacherForm</h1>;
+}
+
+export default TeacherForm;
---
+++ web/src/routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+
+// import { Container } from './styles';
+
+const Routes: React.FC = () => {
+   return (
+      <BrowserRouter>
+         
+      </BrowserRouter>
+   );
+}
+
+export default Routes;
