932daa8da89e18de9269f414f0d6a7efc35b939f
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2020-08-04 16:54:18 -0300

    commit

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
 import './styles.css';
 
 const TeacherForm: React.FC = () => {
-   return <h1>TeacherForm</h1>;
+   return (
+      <div id="page-teacher-form" className="container">
+         <PageHeader title="Que incrivel que você quer dar aulas." />
+      </div>
+   );
 }
 
 export default TeacherForm;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
 import './styles.css';
 
 const TeacherList: React.FC = () => {
    return (
       <div id="page-teacher-list" className="container">
-         <header className="page-header">
-            
-         </header>
+         <PageHeader title="Estes são os proffys disponíveis.">
+            <form id="search-teachers">
+               <div className="input-block">
+                  <label htmlFor="subject">Materia</label>
+                  <input type="text" id="subject" />
+               </div>
+
+               <div className="input-block">
+                  <label htmlFor="week-day">Dia da semana</label>
+                  <input type="text" id="week-day" />
+               </div>
+
+               <div className="input-block">
+                  <label htmlFor="time">Hora</label>
+                  <input type="text" id="time" />
+               </div>
+            </form>
+         </PageHeader>
+
+         <main>
+            <article className="teacher-item">
+               <header>
+                  <img src="https://avatars3.githubusercontent.com/u/39929399?s=460&u=e2f579e1e712cf8ccc31031a924c4579c6574a0b&v=4" alt="Rodrigo" />
+                  <div>
+                     <strong>Rodrigo Pacheco</strong>
+                     <span>Programação</span>
+                  </div>
+               </header>
+
+               <p>
+                  Entusiasta das melhores tecnologias de quimica avançada.
+                     <br /><br />
+                  Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+               </p>
+            </article>
+         </main>
       </div>
    );
 }
