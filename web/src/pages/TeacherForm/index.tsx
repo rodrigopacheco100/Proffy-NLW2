15a76b7c28ed781496c4873bedffd28473aea51e
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2020-08-06 11:04:42 -0300

    commit

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,13 +1,65 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
+import warningIcon from "../../assets/images/icons/warning.svg";
 
 import './styles.css';
+
 
 const TeacherForm: React.FC = () => {
    return (
       <div id="page-teacher-form" className="container">
-         <PageHeader title="Que incrivel que você quer dar aulas." />
+         <PageHeader
+            title="Que incrivel que você quer dar aulas."
+            description="O primeiro passo é preencher esse formulário de inscrição"
+         />
+
+         <main>
+            <fieldset>
+               <legend>Seus Dados</legend>
+
+               <Input name="name" label="Nome completo" />
+               <Input name="avatar" label="Avatar" />
+               <Input name="whatsapp" label="WhatsApp" />
+               <Textarea name="bio" label="Biografia" />
+            </fieldset>
+
+            <fieldset>
+               <legend>Sobre a aula</legend>
+
+               <Select
+                  name="subject"
+                  label="Matéria"
+                  options={[
+                     { value: "Artes", label: "Artes" },
+                     { value: "Biologia", label: "Biologia" },
+                     { value: "Matemática", label: "Matemática" },
+                     { value: "Ciências", label: "Ciências" },
+                     { value: "Educação física", label: "Educação física" },
+                     { value: "Física", label: "Física" },
+                     { value: "Geografia", label: "Geografia" },
+                     { value: "Português", label: "Português" },
+                     { value: "Química", label: "Química" }
+                  ]}
+               />
+               <Input name="cost" label="Custo da sua hora por aula" />
+            </fieldset>
+
+            <footer>
+               <p>
+                  <img src={warningIcon} alt="Aviso importante" />
+                  Importante <br />
+                  Preencha todos os dados
+               </p>
+               <button type="button">
+                  Finalizar cadastro
+               </button>
+            </footer>
+         </main>
       </div>
    );
 }
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 import './styles.css';
 
@@ -10,20 +11,9 @@ const TeacherList: React.FC = () => {
       <div id="page-teacher-list" className="container">
          <PageHeader title="Estes são os proffys disponíveis.">
             <form id="search-teachers">
-               <div className="input-block">
-                  <label htmlFor="subject">Materia</label>
-                  <input type="text" id="subject" />
-               </div>
-
-               <div className="input-block">
-                  <label htmlFor="week-day">Dia da semana</label>
-                  <input type="text" id="week-day" />
-               </div>
-
-               <div className="input-block">
-                  <label htmlFor="time">Hora</label>
-                  <input type="text" id="time" />
-               </div>
+               <Input name="subject" label="Matéria" />
+               <Input name="week-day" label="Dia da semana" />
+               <Input name="time" label="Hora" type="time" />
             </form>
          </PageHeader>
 
