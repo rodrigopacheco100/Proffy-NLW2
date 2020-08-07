51a3c3b8496947f4a81c023500061d1d96378e0e
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2020-08-07 11:00:45 -0300

    commit

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -15,7 +15,8 @@ const Select: React.FC<SelectProps> = ({ name, label, options, ...rest }) => {
    return (
       <div className="select-block">
          <label htmlFor={name}>{label}</label>
-         <select id={name} {...rest}>
+         <select value="" id={name} {...rest}>
+            <option value="" disabled hidden>Selecione uma opção</option>
             {
                options.map(option => {
                   return <option key={option.value} value={option.value}>{option.label}</option>
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
@@ -11,6 +13,14 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 import './styles.css'
 
 const Landing: React.FC = () => {
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            setTotalConnections(response.data.total)
+        })
+    }, [])
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -36,7 +46,7 @@ const Landing: React.FC = () => {
                     </Link>
                 </div>
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="♥" />
+                    Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="♥" />
                 </span>
             </div>
         </div>
