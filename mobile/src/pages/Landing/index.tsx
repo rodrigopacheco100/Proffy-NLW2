9f91413000758a6fc1765432118fdec78c5d82f4
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2020-08-11 09:01:20 -0300

    assets included starting mobile

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const Landing: React.FC = () => {
+   return <View>
+      <Text>a</Text>
+   </View>;
+}
+
+export default Landing;
