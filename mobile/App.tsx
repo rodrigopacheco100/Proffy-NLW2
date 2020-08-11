2b17f9495820c4e772c1186d384bc710e5148d03
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2020-08-11 10:04:37 -0300

    commit

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { AppLoading } from "expo";
+import { useFonts, Archivo_400Regular, Archivo_700Bold } from "@expo-google-fonts/archivo";
+import { Poppins_400Regular, Poppins_600SemiBold } from "@expo-google-fonts/poppins";
 
 import Landing from './src/pages/Landing';
 
 export default function App() {
-  return (
-    <>
-      <Landing />
-      <StatusBar style="auto" />
-    </>
-  );
+  let [fontsLoader] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  })
+
+  if (!fontsLoader) {
+    return <AppLoading />
+  } else {
+    return (
+      <>
+        <Landing />
+        <StatusBar style="auto" />
+      </>
+    );
+  }
 }
--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 
-// import { Container } from './styles';
+import landingImg from "../../assets/images/landing.png";
+
+import styles from './styles';
 
 const Landing: React.FC = () => {
-   return <View>
-      <Text>a</Text>
-   </View>;
+   return (
+      <View style={styles.container}>
+         <Image source={landingImg} style={styles.banner} />
+
+         <Text style={styles.title}>
+            Seja bem-vindo, {'\n'}
+            <Text style={styles.titleBold}>O que deseja fazer?</Text>
+         </Text>
+
+         <View style={styles.buttonsContainer}>
+            <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+               
+            </TouchableOpacity>
+         </View>
+      </View>
+   );
 }
 
 export default Landing;
